4dcb6f80fc47d17b3afcd2d3413e9f5b929831a1
Tejas-coder <tejaskaranjavkar@gmail.com>
2025-06-03 19:47:20 +0530

    Assignment-1

---
+++ Assignment_1/Task1.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+
+print(f"Addition: \t {num1} + {num2} = ",num1+num2)
+print(f"Subtraction: \t {num1} - {num2} = 20",num1-num2)
+print(f"Multiplication: \t {num1} * {num2} = ",num1*num2)
+print(f"Division: \t {num1} / {num2} = ",num1/num2)
